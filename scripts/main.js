11798f631803daebb08ace0d3223ff4bfcd0e14b
LRodriAce <LRodriAce@gmail.com>
2017-07-10 11:35:11 +0200

    Modified main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,32 @@
 'use strict';
 
 const url = 'https://api.github.com/users/guerrero/repos';
-let requestUserRepos = new XMLHttpRequest();
+const requestUserRepos = new XMLHttpRequest();
 requestUserRepos.open('GET', url, true);
 
-requestUserRepos.onload = function() {
+requestUserRepos.onload = () => {
   if (requestUserRepos.status >= 200 && requestUserRepos.status < 400) {
     let dataRepos= JSON.parse(requestUserRepos.responseText);
     let allRepo = [];
 
     for (const elem of dataRepos) {
-      let repoInfo = {
-        name: "",
-        starsCount: 0
-      };
-      repoInfo.name = elem.name || 'Sin nombre';
-      repoInfo.starsCount = elem.stargazers_count || 0;
-      allRepo.push(repoInfo);
+      const {name} = elem || 'Sin nombre';
+      const {stargazers_count} = elem || 0;
+
+      allRepo.push({'name': name, 'starsCount':stargazers_count});
     }
+
     for (const elem of allRepo) {
-      console.log("Name: " + elem.name + " Stars: " + elem.starsCount);
+      console.log(`Name: ${elem.name} Stars: ${elem.starsCount}`);
     }
 
   } else {
     console.log("An error has ocurred");
   }
 };
-requestUserRepos.onerror = function() {
+
+requestUserRepos.onerror = () => {
   console.log("There has been an error");
 };
+
 requestUserRepos.send();
